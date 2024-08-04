65f289019c3ab68559663f2f14d9f055aa87817a
Patrick Arganza <111577207+Patargz12@users.noreply.github.com>
2024-08-04 20:49:38 +0800

    Update openai.js

--- src/openai.js
+++ src/openai.js
@@ -1,4 +1,4 @@
-// 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
+
 
 const { Configuration, OpenAIApi } = require("openai");
 const configuration = new Configuration({
